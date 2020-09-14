b23c74c31b9abe08303c18531c96785eb219103b
Will Harris <will.t.harris@protonmail.com>
2020-09-13 22:21:43 -0700

    initial commit -- next, twin.macro, tippy

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import 'tailwindcss/dist/base.min.css'
+import "tippy.js/dist/tippy.css"
+import "tippy.js/themes/material.css"
+
+const App = ({Component, pageProps}) => <Component {...pageProps} />
+
+export default App
