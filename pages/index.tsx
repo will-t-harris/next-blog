05378cf7592ad6fd2fd54794f5e6bcd08fdca60c
Will Harris <will.t.harris@protonmail.com>
2020-11-08 22:57:05 -0800

    tweak home page styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,28 +10,30 @@ export default function Home() {
     setIsCopied(true)
   }
   return (
-    <main tw="bg-gray-200">
+    <main tw="bg-main min-h-screen text-mainBody">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h2 tw="mb-8 lg:mb-8 mx-auto lg:mx-0 text-4xl font-bold">Hi, I'm Will</h2>
+      <h2 tw="mb-8 lg:mb-8 mx-auto lg:mx-0 text-4xl font-bold text-main">
+        Hi, I'm Will
+      </h2>
       <p tw="mb-8 lg:mb-4 px-8 lg:mx-0 lg:px-0 text-3xl font-medium">
         I'm a software developer, content creator, and bikepacker from San
         Francisco.{" "}
       </p>
       <p tw="lg:mb-4 px-8 lg:mx-0 lg:px-0 text-3xl font-medium">
         I'm currently focused on{" "}
-        <a tw="text-pink-600 hover:underline" href="https://reactjs.org/">
+        <a tw="text-main hover:underline" href="https://reactjs.org/">
           React
         </a>
         ,{" "}
-        <a tw="text-pink-600 hover:underline" href="https://www.gatsbyjs.org/">
+        <a tw="text-main hover:underline" href="https://www.gatsbyjs.org/">
           Gatsby
         </a>
         , and the{" "}
-        <a tw="text-pink-600 hover:underline" href="https://jamstack.org/">
+        <a tw="text-main hover:underline" href="https://jamstack.org/">
           Jamstack
         </a>
         .
@@ -44,7 +46,7 @@ export default function Home() {
         <p tw="px-8 lg:px-0 text-3xl font-medium">
           Want to get in touch? Email me:{" "}
           <CopyToClipboard onCopy={onCopy} text="will@willharris.dev">
-            <span tw="text-pink-600 font-medium cursor-pointer">
+            <span tw="text-main font-medium cursor-pointer">
               will@willharris.dev
             </span>
           </CopyToClipboard>
